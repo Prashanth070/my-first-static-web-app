f29f6de8efdf31d8e4a79a50f9b30f96ff6fc51c
Prashanth070 <144710422+Prashanth070@users.noreply.github.com>
2023-09-11 23:20:21 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import './App.css';
+import Lab1Image from './Lab1.jpg'; 
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div>
+      <p>
+        I did my homework!
+      </p>
+      <img src={Lab1Image} alt="Lab1"/>
+
+    </div>
+  );
 }
 
 export default App;
